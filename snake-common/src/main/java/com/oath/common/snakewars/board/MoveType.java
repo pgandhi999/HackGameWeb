c7a65c4b6719061b8bfee61428d5f1af496404a4
Atul Mohan <atulmohan@yahoo-inc.com>
2018-06-27 23:05:19 -0500

    Add movetype

--- snake-common/src/main/java/com/oath/common/snakewars/board/MoveType.java
+++ snake-common/src/main/java/com/oath/common/snakewars/board/MoveType.java
@@ -2,6 +2,11 @@ package com.oath.common.snakewars.board;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public enum MoveType {
   @JsonProperty("LEFT")
   LEFT,
@@ -12,5 +17,15 @@ public enum MoveType {
   @JsonProperty("DOWN")
   DOWN,
   @JsonProperty("PASS")
-  PASS
+  PASS;
+
+  private static final List<MoveType> movesList = Collections.unmodifiableList(Arrays.asList(values()));
+  private static final Random rand = new Random();
+
+  /*
+   * Generates a random move. This method does not validate the move type and so the returned move may be an invalid one.
+   */
+  public static MoveType getRandomMoveType() {
+    return movesList.get(rand.nextInt(movesList.size()));
+  }
 }
